b7c5bf5c233783243bcbf1b9d97c8c4cb4be065c
Tao0zz <124884467+Tao0zz@users.noreply.github.com>
2024-11-14 14:05:16 -0300

    ticket horario

--- resources/views/dashboard/Professor/create_ticket.blade.php
+++ resources/views/dashboard/Professor/create_ticket.blade.php
@@ -36,68 +36,83 @@
 
         <h2 class="text-3x2 font-black uppercase mb-4">Informações para alteração de horário:</h2>
 
+        
+        
+
         <!-- Form Container -->
         <div class="bg-gray-200 p-8 rounded-md shadow-md w-full max-w-2xl"> <!-- Tornado responsivo com w-full -->
             <h2 class="text-lg font-bold mb-4">Pedido</h2>
-            
-            <!-- Form Grid -->
-            <div class="grid grid-cols-1 md:grid-cols-2 gap-4"> <!-- Responsivo: 1 coluna em telas pequenas, 2 colunas em telas maiores -->
-                <!-- Id do Pedido -->
-                <!-- Funcionário -->
-                <div class="flex flex-col">
-                    <label for="horario_antigo" class="font-bold mb-2">Horário antigo:</label>
-                    <input type="text"  class="form-control bg-gray-200 border border-red-500 p-2 rounded-md w-full" value="{{ $horarioAtual->codigo }} ({{ $horarioAtual->hora_inicio }} - {{ $horarioAtual->hora_fim }})" readonly>
-                    <input type="hidden" name="horario_antigo" value="{{ $horarioAtual->id }}">
-                </div>
-                <div class="flex flex-col">
-                    <label for="horario_novo" class="font-bold mb-2">Horário desejado:</label>
-                    <select name="horario_novo"  class="form-control bg-gray-200 border border-red-500 p-2 rounded-md w-full">
-                    @foreach ($horarios as $horario)
-                    <option value="{{ $horario->id }}">{{ $horario->codigo }} ({{ $horario->hora_inicio }} - {{ $horario->hora_fim }})</option>
+          
+             @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
                     @endforeach
-                    </select>
-                </div>
-
-                <!-- Horário Antigo -->
-                <div class="flex flex-col">
-                    <label for="gestor_responsavel" class="font-bold mb-2">Seu Gerente:</label>
-                    <select name="gestor_responsavel"  class="form-control bg-gray-200 border border-red-500 p-2 rounded-md w-full">
-                    @foreach ($gestores as $gestor)
-                    <option value="{{ $gestor->ID_funcionario }}">{{ $gestor->nome }}</option>
-                    @endforeach
-                </select>
-                </div>
-
-                <!-- Horário Novo -->
-                <div class="flex flex-col">
-                    <label for="descricao" class="font-bold mb-2">Descrição:</label>
-                    <textarea name="descricao" type="text" id="horario-novo" class="form-control bg-gray-200 border border-red-500 p-2 rounded-md w-full">
-                    </textarea>
-                </div>
-
-                <div class="flex flex-col">
-                    <label for="horario-novo" class="font-bold mb-2">Data de Início:</label>
-                    <input type="date" id="horario-novo" class="bg-gray-200 border border-red-500 p-2 rounded-md w-full">
-                </input>
-                </div>
-
-                <div class="flex flex-col">
-                    <label for="horario-novo" class="font-bold mb-2">Data de Fim:</label>
-                    <input type="date" id="horario-novo" class="bg-gray-200 border border-red-500 p-2 rounded-md w-full">
-                </input>
-                </div>
-
-
-
-
-
-                <!-- Confirmar -->
-                <div class="flex flex-col md:col"> <!-- Ocupa 2 colunas em telas maiores -->
-                    <label for="horario-novo" class="font-bold mb-2">‎ </label>
-                    <button class="w-full border bg-red-500 hover:bg-red-700 text-white border-red-500 p-2 rounded-md">Enviar Solicitação</button> <!-- Botão responsivo com w-full -->
-                </div>
+                </ul>
             </div>
+            @endif
+
+            <form action="{{ route('dashboard.professor.store_ticket') }}" method="POST">
+            @csrf
+
+                <!-- Form Grid -->
+                <div class="grid grid-cols-1 md:grid-cols-2 gap-4"> <!-- Responsivo: 1 coluna em telas pequenas, 2 colunas em telas maiores -->
+                    <!-- Id do Pedido -->
+                    <!-- Funcionário -->
+                    <div class="form-group flex flex-col">
+                        <label for="horario_antigo" class="font-bold mb-2">Horário antigo:</label>
+                        <input type="text"  class="form-control bg-gray-200 border border-red-500 p-2 rounded-md w-full" value="{{ $horarioAtual->codigo }} ({{ $horarioAtual->hora_inicio }} - {{ $horarioAtual->hora_fim }})" readonly>
+                        <input type="hidden" name="horario_antigo" value="{{ $horarioAtual->id }}">
+                    </div>
+                    <div class="form-group flex flex-col">
+                        <label for="horario_novo" class="font-bold mb-2">Horário desejado:</label>
+                        <select name="horario_novo"  class="form-control bg-gray-200 border border-red-500 p-2 rounded-md w-full">
+                        @foreach ($horarios as $horario)
+                        <option value="{{ $horario->id }}">{{ $horario->codigo }} ({{ $horario->hora_inicio }} - {{ $horario->hora_fim }})</option>
+                        @endforeach
+                        </select>
+                    </div>
+
+                    <!-- Horário Antigo -->
+                    <div class="form-group flex flex-col">
+                        <label for="gestor_responsavel" class="font-bold mb-2">Seu Gerente:</label>
+                        <select name="gestor_responsavel"  class="form-control bg-gray-200 border border-red-500 p-2 rounded-md w-full">
+                        @foreach ($gestores as $gestor)
+                        <option value="{{ $gestor->ID_funcionario }}">{{ $gestor->nome }}</option>
+                        @endforeach
+                    </select>
+                    </div>
+
+                    <!-- Horário Novo -->
+                    <div class="form-group flex flex-col">
+                        <label for="descricao" class="font-bold mb-2">Descrição:</label>
+                        <textarea name="descricao" type="text" class="form-control bg-gray-200 border border-red-500 p-2 rounded-md w-full">
+                        </textarea>
+                    </div>
+
+                    <div class="form-group flex flex-col">
+                        <label for="data-inicio" class="font-bold mb-2">Data de Início:</label>
+                        <input type="date" name="data_inicio" class="form-control bg-gray-200 border border-red-500 p-2 rounded-md w-full" required>
+                    </input>
+                    </div>
+
+                    <div class="form-group flex flex-col">
+                        <label for="data-fim" class="font-bold mb-2">Data de Fim:</label>
+                        <input type="date" name="data_fim" class="form-control bg-gray-200 border border-red-500 p-2 rounded-md w-full" required>
+                    </input>
+                    </div>
+
+                    <!-- Confirmar -->
+                    <div class="
+                     flex flex-col md:col"> <!-- Ocupa 2 colunas em telas maiores -->
+                        <label class="font-bold mb-2">‎ </label>
+                        <button type="submit" class=" btn btn-primary w-full border bg-red-500 hover:bg-red-700 text-white border-red-500 p-2 rounded-md">Enviar Solicitação</button> <!-- Botão responsivo com w-full -->
+                    </div>
+                </div>
+            </form>
         </div>
+        
     </div>
 
 </body>
@@ -112,8 +127,8 @@
 
 
 
-
-<div class="container">
+{{-- 
+ <div class="container">
     <h1>Solicitar Troca de Horário</h1>
 
     <!-- Exibição de erros de validação -->
@@ -173,4 +188,4 @@
 
         <button type="submit" class="btn btn-primary">Enviar Solicitação</button>
     </form>
-</div>
+</div>  --}}
