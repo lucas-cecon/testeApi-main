99d9b04a5057220d5d8d1f653f44c2f6c0e4c526
Victor <124883537+Vitaixs@users.noreply.github.com>
2024-11-29 06:38:46 -0300

    alterações para fazer rs

--- resources/views/listar.blade.php
+++ resources/views/listar.blade.php
@@ -1,61 +1,51 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lista de Funcionários</title>
+    <title>SENAI - Diplomas</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/header.css') }}">
 </head>
-<body>
+<body class="font-inter bg-gray-100">
+    
+
+    <div class="flex flex-col items-center min-h-screen bg-gray-100 relative"> 
+        @include('components.header', ['sectionTitle' => 'Secretaria', 'pageTitle' => 'Diplomas'])
+
     <h1>Lista de Funcionários</h1>
 
-    <!-- Exibir mensagens de sucesso ou erro -->
-    @if (session('success'))
-        <div style="color: green;">
-            {{ session('success') }}
-        </div>
-    @endif
-
-    @if (session('error'))
-        <div style="color: red;">
-            {{ session('error') }}
-        </div>
-    @endif
-
-    <!-- Exibir informações do token e nome do usuário logado -->
-    @if (session('token') && session('nome') && session('cargo'))
-        <div style="color: blue;">
-            Seu token: {{ session('token') }}<br>
-            Seja bem-vindo, {{ session('nome') }}!<br>
-            Seu cargo: {{ session('cargo') }} <br>
-        </div>
-    @endif
 
     <!-- Botão para cadastrar novo funcionário -->
-    <a href="{{ route('funcionarios.cadastrar') }}">
-        <button style="margin-bottom: 20px;">Cadastrar novo Funcionário</button>
+    <a href="{{ route('funcionarios.cadastrar') }}"><button class="new-order-btn"> <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-4 w-4">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4" />
+                </svg>Cadastrar novo Funcionário</button>
     </a>
+
+    <div class="container mx-auto py-6 w-11/12">
+            <div class="bg-white shadow-md rounded-lg overflow-hidden">
 
     <!-- Campo de pesquisa -->
     <form method="GET" action="{{ route('funcionarios.listar') }}" id="searchForm">
-        <input type="text" id="searchInput" name="search" placeholder="Buscar por nome ou CPF" value="{{ request()->input('search') }}">
-        <button type="submit">Buscar</button>
-        <button type="button" onclick="document.getElementById('searchInput').value = ''; document.getElementById('searchForm').submit();">Limpar</button>
+        <input type="text" class="block w-1/3 p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" id="searchInput" name="search" placeholder="Buscar por nome ou CPF" value="{{ request()->input('search') }}">
+        <button type="submit" class="text-white end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Buscar</button>
+        <button type="button" class="text-white end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onclick="document.getElementById('searchInput').value = ''; document.getElementById('searchForm').submit();">Limpar</button>
     </form>
 
     <!-- Tabela de funcionários -->
-    <table border="1" style="margin-top: 20px;">
-        <thead>
+    <table class="min-w-full table-auto border-collapse border border-gray-200">
+        <thead class="bg-gray-100 text-gray-600 text-sm uppercase font-semibold">
             <tr>
-                <th>ID</th>
-                <th>Nome</th>
-                <th>CPF</th>
-                <th>NIF</th>
-                <th>Cargo</th>
-                <th>Horário</th>
-                <th>Ações</th>
+                <th class="border border-gray-300 px-4 py-2 text-left">ID</th>
+                <th class="border border-gray-300 px-4 py-2 text-left">Nome</th>
+                <th class="border border-gray-300 px-4 py-2 text-left">CPF</th>
+                <th class="border border-gray-300 px-4 py-2 text-left">NIF</th>
+                <th class="border border-gray-300 px-4 py-2 text-left">Cargo</th>
+                <th class="border border-gray-300 px-4 py-2 text-left">Horário</th>
+                <th class="border border-gray-300 px-4 py-2 text-left">Ações</th>
             </tr>
         </thead>
-        <tbody>
+        <tbody class="text-gray-700 text-sm">
             @if($funcionarios->isEmpty())
                 <tr>
                     <td colspan="7">Nenhum funcionário encontrado</td>
@@ -63,13 +53,13 @@
             @else
                 @foreach ($funcionarios as $funcionario)
                     <tr>
-                        <td>{{ $funcionario->ID_funcionario }}</td>
-                        <td>{{ $funcionario->nome }}</td>
-                        <td>{{ $funcionario->cpf }}</td>
-                        <td>{{ $funcionario->nif }}</td>
-                        <td>{{ $funcionario->cargo->descricao ?? 'Cargo não encontrado' }}</td>
-                        <td>{{ $funcionario->horario->hora_inicio ?? 'N/A' }} - {{ $funcionario->horario->hora_fim ?? 'N/A' }}</td>
-                        <td>
+                        <td class="border border-gray-300 px-4 py-2">{{ $funcionario->ID_funcionario }}</td>
+                        <td class="border border-gray-300 px-4 py-2">{{ $funcionario->nome }}</td>
+                        <td class="border border-gray-300 px-4 py-2">{{ $funcionario->cpf }}</td>
+                        <td class="border border-gray-300 px-4 py-2">{{ $funcionario->nif }}</td>
+                        <td class="border border-gray-300 px-4 py-2">{{ $funcionario->cargo->descricao ?? 'Cargo não encontrado' }}</td>
+                        <td class="border border-gray-300 px-4 py-2">{{ $funcionario->horario->hora_inicio ?? 'N/A' }} - {{ $funcionario->horario->hora_fim ?? 'N/A' }}</td>
+                        <td class="border border-gray-300 px-4 py-2">
                             <a href="{{ route('funcionarios.editar', ['id' => $funcionario->ID_funcionario]) }}">Editar</a>
                             <form action="{{ route('api.funcionarios.deletar', ['id' => $funcionario->ID_funcionario]) }}" method="POST" style="display:inline;">
                                 @csrf
@@ -89,10 +79,8 @@
 
     <br>
 
-    <a href="{{ route('perfil') }}">
-        <button style="margin-bottom: 20px;">Ver Perfil</button>
-    </a>
-
-
+</div>
+</div>
+</div>
 </body>
 </html>
