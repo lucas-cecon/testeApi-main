dc010ad43ecc2cc8a14191ae9adfdc0a197450b9
Lucas Cecon <124888722+Cesom-TheRunner@users.noreply.github.com>
2024-12-05 16:52:28 -0300

    sei la

--- resources/views/dashboard/Master/funcionario.blade.php
+++ resources/views/dashboard/Master/funcionario.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SENAI - Diplomas</title>
+    <title>SENAI - Funcionários</title>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/header.css') }}">
 </head>
@@ -27,16 +27,19 @@
     </a>
 
     <div class="container mx-auto py-6 w-11/12">
-            <div class="bg-white shadow-md rounded-lg overflow-hidden">
+            <div class=" shadow-md rounded-lg overflow-hidden">
 
     <!-- Campo de pesquisa -->
-    <form method="GET" action="{{ route('funcionarios.listar') }}" id="searchForm">
-        <input type="text" class="block w-1/3 p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" id="searchInput" name="search" placeholder="Buscar por nome ou CPF" value="{{ request()->input('search') }}">
-        <button type="submit" class="text-white end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Buscar</button>
+    <form method="GET" action="{{ route('funcionarios.listar') }}" id="searchForm" class="flex flex-col md:flex-row items-center justify-center mb-6">
+        <input type="text" class="block w-1/3 p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mr-5" id="searchInput" name="search" placeholder="Buscar por nome ou CPF" value="{{ request()->input('search') }}">
+        <button type="submit" class="text-white end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mr-5">Buscar</button>
         <button type="button" class="text-white end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onclick="document.getElementById('searchInput').value = ''; document.getElementById('searchForm').submit();">Limpar</button>
     </form>
 
-    <!-- Tabela de funcionários -->
+    <!-- Tabela de funcionários --> 
+                                <div class="container mx-auto py-6 w-11/12 ">
+            <div class="bg-white shadow-md rounded-lg overflow-hidden">
+                <div class="overflow-x-auto"> <!-- Permitir rolagem horizontal -->
     <table class="min-w-full table-auto border-collapse border border-gray-200">
         <thead class="bg-gray-100 text-gray-600 text-sm uppercase font-semibold">
             <tr>
